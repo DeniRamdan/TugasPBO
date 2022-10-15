9883ace07645be603cb63af10b2a4ccaadb25e70
DeniRamdan <115355633+DeniRamdan@users.noreply.github.com>
2022-10-16 06:32:29 +0700

    Update Inharitance.java

--- Inharitance.java
+++ Inharitance.java
@@ -2,8 +2,44 @@ package com.sesi3;
 import java.util.Scanner;
 public class Inharitance
 {
+
+   static void display (){
+        System.out.println("===============================");
+        System.out.println("Nama  : Deni Ramdan Septian");
+        System.out.println("Nim   : 20210040055");
+        System.out.println("Kelas : TI21C");
+        System.out.println("===============================================");
+        System.out.println("== TUGAS PEMROGRAMAN BERORIENTASI OBJEK SESI 3 ==");
+        System.out.println("===============================================");
+
+    }
+    static void jumlah(){
+        System.out.println("Jumlah Mahasiswa                  : 4");
+    }
+
+    static void lulus(){
+        System.out.println("Jumlah Mahasiswa yang lulus       : 3 yaitu Budi, Aris, Fahmi");
+    }
+    static void TidakLulus(){
+        System.out.println("Jumlah Mahasiswa yang tidak lulus : 1 yaitu Cahyo");
+    }
+    static void nilai(){
+        System.out.println("Jumlah Mahasiswa dengan nilai A   : 2 yaitu Budi, Fahmi");
+        System.out.println("Jumlah Mahasiswa dengan nilai B   : 1 yaitu Aris");
+        System.out.println("Jumlah Mahasiswa dengan nilai D   : 1 yaitu Cahyo");
+    }
+
+    static void rata(){
+       int rata;
+       double hasil;
+        rata = 80+79+90+50 /4;
+        hasil = rata;
+    }
+
+
    public static void main (String args[])
    {
+    display();
     Scanner input=new Scanner(System.in);
 
     System.out.print("Masukan Jumlah Mahasiswa : ");
@@ -13,6 +49,7 @@ public class Inharitance
     String nama[]=new String[n];
     String status[]=new String[n];
     int nilai[]=new int[n];
+    int jumlah = n;
 
 
     for (int i=0;i<n;i++){
@@ -45,10 +82,11 @@ public class Inharitance
             }else{
                 status[i]="Tidak Lulus";
             }
-
-
+            
     }
-    System.out.println("DAFTAR NILAI MAHASISWA");
+
+
+    System.out.println("== DAFTAR MAHASISWA ==");
     System.out.println("=======================");
     
     for (int i=0; i<n;i++) {
@@ -57,7 +95,12 @@ public class Inharitance
         System.out.println("Nilai   : "+nilai[i]++);
         System.out.println("Stattus : "+status[i]);
         System.out.println("====================");
+       
     }
+    lulus();
+    TidakLulus();
+    nilai();
+    rata();
     
     }
  }
